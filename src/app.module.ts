337e170288aad62ad73bc0e5ee278c7e8412ce39
赵龙生 <zoeyzhao19@gmail.com>
2023-08-15 17:40:04 +0800

    feat: load env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,27 @@ import {TypeOrmModule} from '@nestjs/typeorm'
 import {User} from './domain/users/User'
 import {Permission} from './domain/permissions/Permission'
 import {Role} from './domain/roles/Role'
+import {ConfigModule, ConfigService} from '@nestjs/config'
 
 @Module({
   imports: [
     MediatorModule, 
     IndexModule,
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: "mongodb://root:root@localhost:27017/?authSource=admin",
-      database: 'meeting_room',
-      entities: [User, Permission, Role],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['src/.env', process.env.NODE_ENV === 'development' ? 'src/.env.development' : 'src/.env.production']
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return {
+          type: 'mongodb',
+          url: configService.get('mongo_connection_url'),
+          database: configService.get('mongo_database'),
+          entities: [User, Permission, Role],
+          // logger: process.env.NODE_ENV === 'development' ? 'simple-console' : undefined
+        }
+      },
+      inject: [ConfigService]
     })
   ],
   controllers: [AppController],
