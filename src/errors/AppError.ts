baddddf37aa88def57fb0fa7b5efd61bfa3d7329
赵龙生 <zoeyzhao19@gmail.com>
2023-08-15 18:14:43 +0800

    feat: customize exception filter

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,8 +1,10 @@
-export class AppError extends Error {
+import {HttpException, HttpStatus} from '@nestjs/common'
+
+export class AppError extends HttpException {
   constructor(message: string, ...replacers: string[]) {
     for (const replacer of replacers)
       message = message.replace(/\{.*?\}/, replacer)
-    super(message)
+    super(message, HttpStatus.BAD_REQUEST)
     this.name = this.constructor.name
   }
 }
---
+++ src/filters/exception.filter.ts
@@ -0,0 +1,15 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { Response } from "express";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+
+    return response.json({
+      code: exception.getStatus(),
+      message: exception.message
+    }).end()
+  }
+}
