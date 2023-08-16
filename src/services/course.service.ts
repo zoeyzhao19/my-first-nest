4806b42824c7d93d4dc4a533ca72b704dec159d4
赵龙生 <zoeyzhao19@gmail.com>
2023-08-16 16:50:25 +0800

    chore: update

--- src/services/course.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class CourseService {
-  createCourse(name: string, duration: string): Promise<void> {
-    return Promise.resolve()
-  }
-}
